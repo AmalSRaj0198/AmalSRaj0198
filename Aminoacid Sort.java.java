d4cab48f1cb7f58b7fde27273acf1cdf3ee8052f
Amal S Raj <84905944+AmalSRaj0198@users.noreply.github.com>
2021-05-30 08:55:04 +0530

    Add files via upload

---
+++ Aminoacid Sort.java.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class Sort {
+    public static void main(String args[])
+    {
+    String[] aminoacid={"glycine","alanine","valine","leucine","isoleucine","aspartate","glutamate","serine","threonine","histidine","tyrosine","phenylalanine","cysteine","asparagine","glutamine","proline","tryptophan","methionine","lysine","arginine"};
+    Arrays.sort(amino_acid);
+    System.out.println(Arrays.toString(aminoacid));
+  
+    }
+}
